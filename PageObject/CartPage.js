6e893f49a78faa090a8b3e913be8e2088d7431ef
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2024-12-28 12:12:29 +0800

    update testcase3 checkout method and cartpage class

--- PageObject/CartPage.js
+++ PageObject/CartPage.js
@@ -11,6 +11,20 @@ class CartPage {
     this.continueButton = By.id("continue"); // Continue button
     this.cancelButton = By.id("cancel"); // Cancel button
     this.firstItemName = By.className("inventory_item_name"); // First item name
+    this.errorMessage = By.xpath("//h3[.='Error: First Name is required']"); // Error Msg
+    this.userFirstName = By.id("first-name"); // First name field
+    this.userLastName = By.id("last-name"); // Last name field
+    this.userPostCode = By.id("postal-code"); // Postal code field
+    this.checkoutOverviewTitle = By.className("title"); // Checkout overview title
+    this.paymentInformation = By.xpath("//div[.='Payment Information:']"); // Payment information title
+    this.shippingInformation = By.xpath("//div[.='Shipping Information:']"); // Shipping information title
+    this.priceTotal = By.xpath("//div[.='Price Total']"); // Shipping information title
+    this.summaryTotal = By.xpath("//div[@class='summary_total_label']"); // Summary price total
+    this.finishButton = By.id("finish"); // Finish button
+    this.completeTitle = By.className("title"); // Complete title last page
+    this.completeHeader = By.className("complete-header"); // Complete header confirmation
+    this.backHomeButton = By.id("back-to-products"); // Back home button
+
   }
 
   // Verify item displayed on the cart page
@@ -19,14 +33,13 @@ class CartPage {
     return cartItems.length > 0; // Verify the cart item element should be more than 0, if there's item in it
   }
 
+  // Verify fist item name displayed
   async isFirstItemDisplayed() {
     const firstItem = await this.driver.findElement(this.firstItemName);
     return firstItem.getText();
-}
+  }
 
-
-
-  // Verify checkout method
+  // Verify checkout button click
   async isCheckoutBtnClick() {
     const checkoutBtn = await this.driver.findElement(this.checkoutButton);
     await checkoutBtn.click();
@@ -38,17 +51,83 @@ class CartPage {
     return infoTitle.getText();
   }
 
-  // Verify continue button
-  async isContinueBtnDisplayed() {
+  // Verify continue button click
+  async isContinueBtnClick() {
     const continueBtn = await this.driver.findElement(this.continueButton);
     await continueBtn.click();
   }
 
-  // Verify continue button
+  // Verify cancel button displayed
   async isCancelBtnDisplayed() {
     const cancelBtn = await this.driver.findElement(this.cancelButton);
-    return await cancelBtn.isCancelBtnDisplayed();
+    return await cancelBtn.isDisplayed();
+  }
+
+  // Verify error msg
+  async isErrorMsgDisplayed() {
+    const errorMsg = await this.driver.findElement(this.errorMessage);
+    return errorMsg.getText();
+  }
+
+  // Veriry fill in the information fields
+  async fillInformationFields(firstname, lastname, postcode) {
+    await this.driver.findElement(this.userFirstName).sendKeys(firstname);
+    await this.driver.findElement(this.userLastName).sendKeys(lastname);
+    await this.driver.findElement(this.userPostCode).sendKeys(postcode);
+  }
+
+  // Verify Overview title displayed
+  async isCheckoutOverviewTitle() {
+    const overviewtitle = await this.driver.findElement(this.checkoutOverviewTitle);
+    return overviewtitle.getText();
+  }
+
+  // Verify payment information displayed
+  async isPaymentInfoDisplayed() {
+    const paymentInfo = await this.driver.findElement(this.paymentInformation);
+    return await paymentInfo.isDisplayed();
+  }
+
+  // Verify shipping information displayed
+  async isShippingInfoDisplayed() {
+    const shippingInfo = await this.driver.findElement(this.shippingInformation);
+    return await shippingInfo.isDisplayed();
+  }
+
+  // Verify shipping information displayed
+  async isPriceTotalDisplayed() {
+    const pricingTotal = await this.driver.findElement(this.priceTotal);
+    return await pricingTotal.isDisplayed();
+  }
+
+  // Verify summary total displayed
+  async isSummaryTotalDisplayed() {
+    const summaryTotal = await this.driver.findElement(this.summaryTotal);
+    return await summaryTotal.isDisplayed();
+  }
+
+  // Verify finish button click
+  async isFinishBtnClick() {
+    const finishBtn = await this.driver.findElement(this.finishButton);
+    await finishBtn.click();
+  }
+
+  // Verify shipping information displayed
+  async isCompleteTitleDisplayed() {
+    const completeTitle = await this.driver.findElement(this.completeTitle);
+    return completeTitle.getText();
+  }
+
+  // Verify shipping information displayed
+  async isCompleteHeaderDisplayed() {
+    const completedHeader = await this.driver.findElement(this.completeHeader);
+    return completedHeader.getText();
+  }
+
+  // Verify back home button click
+  async isBackHomeBtnClick() {
+    const BackHomeBtn = await this.driver.findElement(this.backHomeButton);
+    await BackHomeBtn.click();
   }
 }
-
 module.exports = CartPage;
--- Test/TestCase3.js
+++ Test/TestCase3.js
@@ -1,42 +1,137 @@
 const { Builder } = require("selenium-webdriver");
 const LoginPage = require("../PageObject/LoginPage");
-const DashboardPage = require("../PageObject/Dashboard");
 const CartPage = require("../PageObject/CartPage");
 const { expect } = require("chai");
 const fs = require("fs");
+const assert = require("assert");
+const DashboardPage = require("../PageObject/Dashboard");
+require("dotenv").config();
 
-describe('Test Case 3 [Verify Item in Cart]', function () {
+const browser = process.env.BROWSER;
+const baseUrl = process.env.BASE_URL;
+const username = process.env.USER_NAME;
+const passwword = process.env.PASSWORD;
+const firstname = process.env.FIRST_NAME;
+const lastname = process.env.LAST_NAME;
+const postcode = process.env.POST_CODE;
+
+const screenshotDir = "./screenshots/";
+if (!fs.existsSync(screenshotDir)) {
+  fs.mkdirSync(screenshotDir, { recursive: true });
+}
+
+describe("Test Case 3 [Checkout method]", function () {
   this.timeout(40000);
   let driver;
-  let loginPage;
-  let dashboardPage;
-  let cartPage;
 
-  const screenshotDir = "./screenshots/";
-  if (!fs.existsSync(screenshotDir)) {
-    fs.mkdirSync(screenshotDir, { recursive: true });
+  switch (browser.toLowerCase()) {
+    case "firefox":
+      const firefox = require("selenium-webdriver/firefox");
+      options = new firefox.Options();
+      options.addArguments("--headless");
+    case "edge":
+      const edge = require("selenium-webdriver/edge");
+      options = new edge.Options();
+      options.addArguments("--headless");
+    case "chrome":
+    default:
+      const chrome = require("selenium-webdriver/chrome");
+      options = new chrome.Options();
+      options.addArguments("--headless");
+      break;
   }
 
   before(async function () {
-    driver = await new Builder().forBrowser("chrome").build();
-    loginPage = new LoginPage(driver);
-    dashboardPage = new DashboardPage(driver);
-    cartPage = new CartPage(driver);
+    driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
+  });
 
-    await driver.get("https://www.saucedemo.com");
-    await loginPage.login("standard_user", "secret_sauce");
+  beforeEach(async function () {
+    const loginPage = new LoginPage(driver);
+    await loginPage.navigate(baseUrl);
+    await loginPage.login(username, passwword);
   });
 
   it("Verify checkout method successful", async function () {
-    // Verify add item to cart
+    const dashboardPage = new DashboardPage(driver);
+    const cartPage = new CartPage(driver);
+
+    // Verify add item to the cart
     await dashboardPage.addItemToCart();
 
-    // Verify go to cart page
+    // Verify cart badge displayed
+    await dashboardPage.isCartBadgetVisible();
+
+    // Verify go to the cart page
     await dashboardPage.goToCart();
 
-    // Verify item displayed on cart page
-    const isItemInCart = await cartPage.isItemInCart();
-    expect(isItemInCart).to.be.true;
+    // Verify item displayed on the cart page
+    const cartItems = await cartPage.isItemInCart();
+    expect(cartItems).to.be.true;
+
+    // Verify first item name displayed correctly
+    const firstItem = await cartPage.isFirstItemDisplayed();
+    assert.strictEqual(firstItem, "Sauce Labs Backpack", "Product name does not match!");
+
+    // Verify click checkout button
+    await cartPage.isCheckoutBtnClick();
+
+    // Verify your information displayed
+    const informationTitle = await cartPage.isInformationTitle();
+    assert.strictEqual(informationTitle, "Checkout: Your Information", "Information title is not displayed!");
+
+    // Verify click continue button
+    await cartPage.isContinueBtnClick();
+
+    // Verify error msg displayed
+    const errorMessage = await cartPage.isErrorMsgDisplayed();
+    assert.strictEqual(errorMessage, "Error: First Name is required", "Expected error message does not match");
+
+    // Veriry fill in the information fields
+    await cartPage.fillInformationFields(firstname, lastname, postcode);
+
+    // Verify click checkout button
+    await cartPage.isContinueBtnClick();
+
+    // Verify Overview title displayed
+    const overviewtitle = await cartPage.isCheckoutOverviewTitle();
+    assert.strictEqual(overviewtitle, "Checkout: Overview", "Checkout overview title is not displayed!");
+
+    // Verify item displayed
+    await cartPage.isItemInCart();
+
+    // Verify first item name displayed correctly
+    const checkoutItem = await cartPage.isFirstItemDisplayed();
+    assert.strictEqual(checkoutItem, "Sauce Labs Backpack", "Product name does not match!");
+
+    // Verify payment information displayed correctly
+    await cartPage.isPaymentInfoDisplayed();
+
+    // Verify payment information displayed correctly
+    await cartPage.isShippingInfoDisplayed();
+
+    // Verify price total displayed correctly
+    await cartPage.isPriceTotalDisplayed();
+
+
+    // Verify summary total displayed
+    await cartPage.isSummaryTotalDisplayed();
+
+    // Verify Cancel button displayed
+    await cartPage.isCancelBtnDisplayed();
+
+    // Verify finish button clicked
+    await cartPage.isFinishBtnClick();
+
+    // Verify complete page title displayed correctly
+    const completeTitle = await cartPage.isCompleteTitleDisplayed();
+    assert.strictEqual(completeTitle, "Checkout: Complete!", "Complete page tittle does not match!");
+
+    // Verify complete page title displayed correctly
+    const completedHeader = await cartPage.isCompleteHeaderDisplayed();
+    assert.strictEqual(completedHeader, "Thank you for your order!", "Complete header text does not match!");
+
+    // Verify back home button clicked
+    // await cartPage.isBackHomeBtnClick();
   });
 
   afterEach(async function () {
